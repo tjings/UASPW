b7243b83a94f3deac433978d03dec4d6112540b6
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-11 11:28:58 +0700

    Tambah beberapa tabel baru.

--- assets/uas.sql
+++ assets/uas.sql
@@ -1,70 +1,63 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Waktu pembuatan: 10 Bulan Mei 2020 pada 18.19
--- Versi server: 10.4.6-MariaDB
--- Versi PHP: 7.3.9
+DROP DATABASE IF EXISTS uas;
+CREATE DATABASE IF NOT EXISTS uas;
+USE uas;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE login (
+  id_user INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  nama_user VARCHAR(255) NOT NULL,
+  role ENUM('admin', 'user') NOT NULL,
+  PRIMARY KEY (id_user)
+)
+ENGINE = InnoDB;
 
+CREATE TABLE movies (
+  id_movie INT NOT NULL AUTO_INCREMENT,
+  nama_movie VARCHAR(255) NOT NULL,
+  genre_movie VARCHAR(255) NOT NULL,
+  sinopsis TEXT NOT NULL,
+  minimal_usia INT NOT NULL,
+  release_date DATE NOT NULL,
+  waktu_film INT NOT NULL,
+  poster_movie VARCHAR(255),
+  harga_movie INT NOT NULL,
+  PRIMARY KEY (id_movie)
+)
+ENGINE = InnoDB;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE ruangan (
+  id_ruangan INT NOT NULL AUTO_INCREMENT,
+  nama_ruangan VARCHAR(255),
+  PRIMARY KEY (id_ruangan)
+)
+ENGINE = InnoDB;
 
---
--- Database: `uas`
---
+CREATE TABLE kursi (
+  id_kursi VARCHAR(255) NOT NULL,
+  id_ruangan INT NOT NULL,
+  PRIMARY KEY (id_kursi)
+  FOREIGN KEY (id_ruangan) REFERENCES ruangan(id_ruangan)
+)
+ENGINE = InnoDB;
 
--- --------------------------------------------------------
+CREATE TABLE orders (
+  id_order INT NOT NULL AUTO_INCREMENT,
+  id_user INT NOT NULL,
+  id_movie INT NOT NULL,
+  id_ruangan INT NOT NULL,
+  id_kursi VARCHAR(255) NOT NULL,
+  jumlah_tiket INT NOT NULL,
+  no_kursi INT NOT NULL,
+  total_pembayaran INT NOT NULL,
+  PRIMARY KEY (id_order),
+  FOREIGN KEY (id_user) REFERENCES login(user_id),
+  FOREIGN KEY (id_movie) REFERENCES movies(id_movie),
+  FOREIGN KEY (id_ruangan) REFERENCES ruangan(id_ruangan),
+  FOREIGN KEY (id_kursi) REFERENCES kursi(id_kursi)
+)
+ENGINE = InnoDB;
 
---
--- Struktur dari tabel `login`
---
-
-CREATE TABLE `login` (
-  `id_user` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `nama_user` varchar(100) NOT NULL,
-  `role` enum('admin','user') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `login`
---
-
-INSERT INTO `login` (`id_user`, `username`, `password`, `nama_user`, `role`) VALUES
+INSERT INTO login VALUES
 (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'admin'),
 (2, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'user', 'user');
-
---
--- Indexes for dumped tables
---
-
---
--- Indeks untuk tabel `login`
---
-ALTER TABLE `login`
-  ADD PRIMARY KEY (`id_user`);
-
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
-
---
--- AUTO_INCREMENT untuk tabel `login`
---
-ALTER TABLE `login`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
