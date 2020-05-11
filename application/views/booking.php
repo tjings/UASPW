e7a1f718e7f68ca2e5fddf9d8097139b0c880c9e
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-11 12:43:37 +0700

    Modifikasi dashboard + tambah UI booking.

---
+++ application/views/booking.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Book</title>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <div class="container">
+      <h1>Movie Poster</h1>
+      <img src="<?php echo base_url() . $movie['poster_movie']; ?>">
+
+      <h2>Data Movie</h2>
+      <?php
+        $id = $movie['id_movie'];
+        $nama = $movie['nama_movie'];
+        $genre = $movie['genre_movie'];
+        $sinopsis = $movie['sinopsis'];
+        $usia = $movie['minimal_usia'];
+        $release = $movie['release_date'];
+        $lama = $movie['waktu_film'];
+        $harga = $movie['harga_movie'];
+
+        echo "<p>ID: $id</p>";
+        echo "<p>Nama: $nama</p>";
+        echo "<p>Genre: $genre</p>";
+        echo "<p>Sinopsis: $sinopsis</p>";
+        echo "<p>Usia: $usia</p>";
+        echo "<p>Release Date: $release</p>";
+        echo "<p>Lama Movie: $lama</p>";
+        echo "<p>Harga: $harga</p>";
+      ?>
+
+      <h2>Pesan</h2>
+      <a href="<?php echo site_url(); ?>">Kembali</a>
+
+    </div>
+  
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </body>
+</html>
--- application/views/user/dashboard.php
+++ application/views/user/dashboard.php
@@ -4,7 +4,8 @@
     <title>Welcome</title>
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs/dt-1.10.20/r-2.2.3/datatables.min.css"/> 
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() . "assets/css/style.css"; ?>">
 </head>
 <body>
 <nav class="navbar navbar-inverse navbar-fixed-top">
@@ -31,7 +32,7 @@
               <a href="#" class="list-group-item active" style="text-align: center;background-color: black;border-color: black">
                 USER
               </a>
-              <a href="dashboard.php" class="list-group-item"><i class="fa fa-dashboard"></i> Dashboard</a>
+              <a href="<?php echo site_url() . "/dashboard"; ?>" class="list-group-item"><i class="fa fa-dashboard"></i> Dashboard</a>
               <a href="#" class="list-group-item"><i class="fa fa-book"></i> Blog</a>
               <a href="#" class="list-group-item"><i class="fa fa-folder"></i> Kategori</a>
               <a href="#" class="list-group-item"><i class="fa fa-comments-o"></i> Komentar</a>
@@ -51,7 +52,52 @@
     </div>
 </div>
 
+<div class="container">
+  <h3 class='text-center'>Seluruh Film</h3>
+  <table id="movie" class="table table-striped table-bordered" style="width:100%;">
+    <thead>
+      <tr>
+        <th>No.</th>
+        <th>Nama</th>
+        <th>Genre</th>
+        <th>Minimal Usia</th>
+        <th>Release Date</th>
+        <th>Lama Film</th>
+        <th>Aksi</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+        $counter = 1;
+        foreach($movies as $movie) {
+          $id = $movie['id_movie'];
+          $name = $movie['nama_movie'];
+          $genre = $movie['genre_movie'];
+          $usia = $movie['minimal_usia'];
+          $release = $movie['release_date'];
+          $lama = $movie['waktu_film'];
+          $pathToOrder = site_url() . "/booking/showBooking/$id";
+
+          echo "<tr>";
+            echo "<td>$counter</td>";
+            echo "<td>$name</td>";
+            echo "<td>$genre</td>";
+            echo "<td>$usia</td>";
+            echo "<td>$release</td>";
+            echo "<td>$lama jam</td>";
+            echo "<td><a href=$pathToOrder>Pesan</a></td>";
+          echo "</tr>";
+
+          $counter += 1;
+        }
+      ?>
+    </tbody>
+  </table>
+</div>
+
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs/dt-1.10.20/r-2.2.3/datatables.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url() . "assets/js/script.js" ?>"></script>
 </body>
 </html>
