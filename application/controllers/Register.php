3aa2778dbd361bb0e8b8d216093c4ae2f5a01f8c
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-14 16:39:19 +0700

    Membetulkan bug dalam proses registrasi.

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -7,28 +7,34 @@
          parent::__construct();
          $this->load->library(array('form_validation'));
          $this->load->helper(array('url','form'));
-         $this->load->model('register'); 
+         $this->load->model('Register_model'); 
+     }
+
+     public function index()
+     {
+         $this->load->view('register');
      }
  
      public function registrasi() {
- 
          $this->form_validation->set_rules('name', 'NAME','required');
          $this->form_validation->set_rules('username', 'USERNAME','required');
          $this->form_validation->set_rules('password','PASSWORD','required');
          $this->form_validation->set_rules('password_conf','PASSWORD','required|matches[password]');
+         
          if($this->form_validation->run() == FALSE) {
-             $this->load->view('register');
-         }else{
- 
-             $data['nama']   =    $this->input->post('name');
+            redirect('register');
+         }
+         else {
              $data['username'] =    $this->input->post('username');
              $data['password'] =    md5($this->input->post('password'));
+             $data['nama_user']   =    $this->input->post('name');
+             $data['role'] = 'user';
  
-             $this->register->daftar($data);
+             $this->Register_model->daftar($data);
              
-             $pesan['message'] =    "Pendaftaran berhasil";
+             $pesan['message'] = "Pendaftaran berhasil";
              
-             $this->load->view('success',$pesan);
+             $this->load->view('success', $pesan);
          }
      }
  }
