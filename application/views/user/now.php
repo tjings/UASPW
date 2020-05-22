cf69e4d7326bd1b827c781cbe02d07ed4e33b417
Acauu <64388690+Acauu@users.noreply.github.com>
2020-05-22 23:05:44 +0800

    now
    now.php

--- application/views/user/now.php
+++ application/views/user/now.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html>
     <head>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs/dt-1.10.20/r-2.2.3/datatables.min.css"/> 
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() . "assets/css/dashboard.css"; ?>">
+</head>
         <title>Now Playing</title>
     </head>
     <body>
@@ -22,14 +31,15 @@
                 <tbody>
                     <?php
                         $counter = 1;
-                        foreach($movies as $movie) {
-                            $id = $movie['id_movie'];
-                            $name = $movie['nama_movie'];
-                            $genre = $movie['genre_movie'];
-                            $usia = $movie['minimal_usia'];
-                            $release = $movie['release_date'];
-                            $lama = $movie['waktu_film'];
-                            $ruangan = $movie['id_ruangan'];
+                        
+                        foreach((array) $data as $i) {
+                            $id = $i['id_movie'];
+                            $name = $i['nama_movie'];
+                            $genre = $i['genre_movie'];
+                            $usia = $i['minimal_usia'];
+                            $release = $i['release_date'];
+                            $lama = $i['waktu_film'];
+                            $ruangan = $i['id_ruangan'];
                             $pathToOrder = site_url() . "/booking/showBooking?id=$id&ruangan=$ruangan";
 
                         echo "<tr>";
