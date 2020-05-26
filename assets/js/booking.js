ca9a8f4ed6bba487bfa5f9c35e7bbfd319553d36
lauslim12 <nicholasdwiarto@yahoo.com>
2020-05-26 15:43:25 +0700

    Fitur booking kursi per-ruangan selesai.

--- assets/js/booking.js
+++ assets/js/booking.js
@@ -1,1 +1,17 @@
-// nanti kerjain lagi
+// ES6 Syntax
+const errorMessage = () => alert("Kursi sudah dibooking!");
+
+const toggleWarna = async () => {
+  let kursi = document.querySelectorAll(".box");
+
+  for(let i = 0; i < kursi.length; i++) {
+    kursi[i].classList.remove("box-dipilih");
+  }
+}
+
+const ubahWarna = async (x) => {
+  await toggleWarna();
+  x.classList.add("box-dipilih");
+  let nomorKursi = x.getAttribute('data-value');
+  document.getElementById("tempatKursi").value = nomorKursi;
+}
