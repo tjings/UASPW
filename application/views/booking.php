89275e4a7b74497d21f94f93be85e608e6ff8b20
rxgvnd <ira.suryani@student.umn.ac.id>
2020-05-30 14:19:49 +0800

    navbar & landing page

--- application/views/booking.php
+++ application/views/booking.php
@@ -3,10 +3,6 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Book</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
@@ -15,35 +11,24 @@
     <script type="text/javascript" src="<?php echo base_url() . "assets/js/booking.js"; ?>"></script>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-fixed-top" style="background-color: #114B4C;" >
-    <button class="navbar-toggler"type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <img src="https://pngimage.net/wp-content/uploads/2018/06/white-menu-icon-png-8.png" width="40" height="35" class="d-inline-block-align-top" alt="Image Logo"/>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <a style="color: #FFFFFF" class="navbar-brand" href="<?php echo site_url('dashboard')?>">
-          <img src="<?= base_url() . "/assets/images/logo.jpg"; ?>" style="border-radius: 25px;" width="40" height="35" class="d-inline-block-align-top" alt="Image Logo"/>
-        </a>
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a style="color: #FFFFFF" class="nav-link text-light" href="<?php echo site_url('booking/now') ?>"> Now Playing</a>
-            </li>
-            <li class="nav-item">
-                <a style="color: #FFFFFF" class="nav-link text-light" href="<?php echo site_url('booking/teater') ?>">Theaters</a>
-            </li>
-            <li class="nav-item">
-                <a style="color: #FFFFFF" class="nav-link text-light" href="<?php echo site_url('booking/soon') ?>">Coming Soon</a>
-            </li>
-            <li class="nav-item">
-                <a style="color: #FFFFFF" class="nav-link text-light" href="<?php echo site_url('booking/EditUser') ?>">User</a>
-            </li>
+<header>
+    <a href="<?php echo site_url('dashboard')?>">
+        <img class="logo" src="<?= base_url() . "/assets/images/logo.jpg"; ?>" style="border-radius: 25px;" width="40" height="35" alt="logo">
+    </a>
+    <nav>
+        <ul class="nav_links">
+            <li><a href="<?php echo site_url('booking/now') ?>">Now Playing</a></li>
+            <li><a href="<?php echo site_url('booking/teater') ?>">Theaters</a></li>
+            <li><a href="<?php echo site_url('booking/soon') ?>">Coming Soon</a></li>
         </ul>
-    </div>
-    <div id="navbar" class="navbar-collapse collapse">
-        <div class="navbar-form navbar-right">
-            <a href="<?php echo base_url() ?>index.php/dashboard/logout" type="submit" class="btn btn-light"><i class="fa fa-sign-out"></i> Logout</a>
-        </div>
-    </div>
-</nav>
+    </nav>
+    <li class="dropdown"><a href="#"><button>Profile</button></a>
+        <ul class="dropdown-content">
+            <li><a href="<?php echo site_url('booking/EditUser') ?>">Edit Profile</a></li>
+            <li><a href="<?php echo base_url() ?>index.php/dashboard/logout">Log Out</a></li>
+        </ul>
+    </li>
+</header>
 <div class="container" style="padding-top: 50px;">
   <?php
     $id = $movie['id_movie'];
--- application/views/user/coming.php
+++ application/views/user/coming.php
@@ -3,41 +3,27 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Coming Soon</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() . "assets/css/dashboard.css"; ?>">  
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-fixed-top" style="background-color: #114B4C;" >
-    <button class="navbar-toggler"type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <img src="https://pngimage.net/wp-content/uploads/2018/06/white-menu-icon-png-8.png" width="40" height="35" class="d-inline-block-align-top" alt="Image Logo"/>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <a style="color: #FFFFFF" class="navbar-brand" href="<?php echo site_url('dashboard')?>">
-        <img src="<?= base_url() . "/assets/images/logo.jpg"; ?>" style="border-radius: 25px;" width="40" height="35" class="d-inline-block-align-top" alt="Image Logo"/></a>
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a style="color: #FFFFFF" class="nav-link text-light" href="<?php echo site_url('booking/now') ?>"> Now Playing</a>
-            </li>
-            <li class="nav-item">
-                <a style="color: #FFFFFF" class="nav-link text-light" href="<?php echo site_url('booking/teater') ?>">Theaters</a>
-            </li>
-            <li class="nav-item">
-                <a style="color: #FFFFFF" class="nav-link text-light" href="<?php echo site_url('booking/soon') ?>">Coming Soon</a>
-            </li>
-            <li class="nav-item">
-                <a style="color: #FFFFFF" class="nav-link text-light" href="<?php echo site_url('booking/EditUser') ?>">User</a>
-            </li>
+<header>
+    <a href="<?php echo site_url('dashboard')?>">
+        <img class="logo" src="<?= base_url() . "/assets/images/logo.jpg"; ?>" style="border-radius: 25px;" width="40" height="35" alt="logo">
+    </a>
+    <nav>
+        <ul class="nav_links">
+            <li><a href="<?php echo site_url('booking/now') ?>">Now Playing</a></li>
+            <li><a href="<?php echo site_url('booking/teater') ?>">Theaters</a></li>
+            <li><a href="<?php echo site_url('booking/soon') ?>">Coming Soon</a></li>
         </ul>
-    </div>
-    <div id="navbar" class="navbar-collapse collapse">
-        <div class="navbar-form navbar-right">
-            <a href="<?php echo base_url() ?>index.php/dashboard/logout" type="submit" class="btn btn-light"><i class="fa fa-sign-out"></i> Logout</a>
-        </div>
-    </div>
-</nav>
+    </nav>
+    <li class="dropdown"><a href="#"><button>Profile</button></a>
+        <ul class="dropdown-content">
+            <li><a href="<?php echo site_url('booking/EditUser') ?>">Edit Profile</a></li>
+            <li><a href="<?php echo base_url() ?>index.php/dashboard/logout">Log Out</a></li>
+        </ul>
+    </li>
+</header>
     <div class="container" style="padding-top: 100px;">
         <div class="row"  style="box-shadow: 2px 2px 2px 2px rgba(0,0,0,0.3);">
             <?php foreach ((array) $data as $i) : 
