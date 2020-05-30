022d59fbefbc6aa8f380ee01ca2b9eca4a47e0e6
rxgvnd <ira.suryani@student.umn.ac.id>
2020-05-31 01:59:38 +0800

    view search

--- application/views/user/now.php
+++ application/views/user/now.php
@@ -4,7 +4,13 @@
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Now Playing</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="path/to/material-design-iconic-font/css/material-design-iconic-font.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() . "assets/css/now.css"; ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() . "assets/css/kursi.css"; ?>">
     </head>
 <body>
 <header>
@@ -25,6 +31,10 @@
             </li>
         </ul>
     </nav>
+    <form action="<?= site_url(). "/booking/search"; ?>" method="GET">
+        <input type="text" name="search" placeholder="Search...">
+        <button type="submit"style="background-color: transparent; min-width:0px; margin-left:90%;"><i class="zmdi zmdi-search" style="color: white;"></i></button>
+    </form>
     <li class="dropdown"><a href="#"><button>Profile</button></a>
         <ul class="dropdown-content">
             <li><a href="<?php echo site_url('booking/EditUser') ?>">Edit Profile</a></li>
