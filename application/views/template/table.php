c667b7d6de38a7530fc7cc6b0e850734dab0ad1d
chatyhuang <chaterine@astudent.umn.ac.id>
2020-05-31 14:19:11 +0700

    update table.php

--- application/views/template/table.php
+++ application/views/template/table.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?php echo base_url() . "assets/css/table.css" ?>">
+</head>
+<body>
+<div class="container-table">
 <table id="tblMovie" class="table table-striped table-bordered" cellspacing="0" width="100%">
 	<thead>
 		<tr>
@@ -66,3 +76,6 @@
     <td>Aksi</td>
 	</tfoot>
 </table>
+</div>
+</body>
+</html>
